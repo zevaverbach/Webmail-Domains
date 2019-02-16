ff7304a0070f2569ef762c27822e5943ebadef48
zevav <zev@averba.ch>
2019-02-16 08:44:47 -0500

    ../..

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as file:
 setup(
     name="webmail-domains",
     version="0.17",
-    py_modules=["webmail_domains"],
+    py_modules=["webmail_domains.webmail_domains"],
     url='https://github.com/zevaverbach/Webmail-Domains',
     description=('A pretty thorough list of webmail domains!'),
     long_description_content_type='text/markdown',
